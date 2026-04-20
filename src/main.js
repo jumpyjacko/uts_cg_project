d293a3a327da114dbbc3a7c4072505df464bdfa1
Jackson Ly <bobjacklee27@gmail.com>
2026-04-20 12:58:54 +1000

    refactor: move sky to separate file

--- src/main.js
+++ src/main.js
@@ -1,60 +1,13 @@
 import * as THREE from 'three';
+
 import { World } from './world.js';
+import { terrain } from './terrain.js';
+import { Sky } from './sky.js';
 
 let world = new World(true); // true is enabling some debug renderers
 
 // setup lighting and sky
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 2);
-hemiLight.color.setHex(0x7098FE);
-hemiLight.groundColor.setHex(0xf7f9ff);
-hemiLight.position.set(0, 50, 0);
-world.add(hemiLight);
-
-const hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 10); // debug
-world.add(hemiLightHelper);
-
-const spotlight = new THREE.SpotLight(0xFFF9FF, 3);
-spotlight.position.set(-1, 1.75, 1);
-spotlight.position.multiplyScalar(30);
-spotlight.power = 17000;
-spotlight.angle = 70;
-world.add(spotlight);
-
-spotlight.castShadow = true;
-spotlight.shadow.mapSize.width = 8192;
-spotlight.shadow.mapSize.height = 8192;
-
-const d = 50;
-spotlight.shadow.camera.left = -d;
-spotlight.shadow.camera.right = d;
-spotlight.shadow.camera.top = d;
-spotlight.shadow.camera.bottom = -d;
-spotlight.shadow.camera.far = 500;
-spotlight.shadow.bias = -0.00;
-
-const dirLightHelper = new THREE.SpotLightHelper(spotlight, 10); // debug
-world.add(dirLightHelper);
-
-// skydome
-import skyVertShader from './shaders/skyDome.vert?raw';
-import skyFragShader from './shaders/skyDome.frag?raw';
-import { terrain } from './terrain.js';
-const skyUniforms = {
-    'topColor': { value: new THREE.Color(0x5078FE) },
-    'bottomColor': { value: new THREE.Color(0xf7f9ff) },
-    'offset': { value: 33 },
-    'exponent': { value: 0.6 }
-};
-skyUniforms['topColor'].value.copy(hemiLight.color);
-world.scene.fog.color.copy(skyUniforms['bottomColor'].value);
-const skyGeo = new THREE.SphereGeometry(200, 32, 15);
-const skyMat = new THREE.ShaderMaterial({
-    uniforms: skyUniforms,
-    vertexShader: skyVertShader,
-    fragmentShader: skyFragShader,
-    side: THREE.BackSide
-});
-const sky = new THREE.Mesh(skyGeo, skyMat);
+let sky = new Sky();
 world.add(sky);
 
 // Island terrain
---
+++ src/sky.js
@@ -0,0 +1,67 @@
+import * as THREE from 'three';
+
+import skyVertShader from './shaders/skyDome.vert?raw';
+import skyFragShader from './shaders/skyDome.frag?raw';
+
+export class Sky {
+    constructor(colour = 0x7098fe) {
+        this.sky_colour = colour;
+        this.fog_colour = 0xf7f9ff;
+
+        this.create();
+
+        return this.group;
+    }
+
+    create() {
+        this.group = new THREE.Group();
+
+        // lighting
+        const hemiLight = new THREE.HemisphereLight(this.sky_colour, this.fog_colour, 2);
+        hemiLight.position.set(0, 50, 0);
+        this.group.add(hemiLight);
+
+        const hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 10); // debug
+        this.group.add(hemiLightHelper);
+
+        const spotlight = new THREE.SpotLight(0xFFF9FF, 3);
+        spotlight.position.set(-1, 1.75, 1);
+        spotlight.position.multiplyScalar(30);
+        spotlight.power = 17000;
+        spotlight.angle = 70;
+        this.group.add(spotlight);
+
+        spotlight.castShadow = true;
+        spotlight.shadow.mapSize.width = 8192;
+        spotlight.shadow.mapSize.height = 8192;
+
+        const d = 50;
+        spotlight.shadow.camera.left = -d;
+        spotlight.shadow.camera.right = d;
+        spotlight.shadow.camera.top = d;
+        spotlight.shadow.camera.bottom = -d;
+        spotlight.shadow.camera.far = 500;
+        spotlight.shadow.bias = -0.00;
+
+        const dirLightHelper = new THREE.SpotLightHelper(spotlight, 10); // debug
+        this.group.add(dirLightHelper);
+
+        // skydome
+        const skyUniforms = {
+            'topColor': { value: new THREE.Color(this.sky_colour) },
+            'bottomColor': { value: new THREE.Color(this.fog_colour) },
+            'offset': { value: 33 },
+            'exponent': { value: 0.6 }
+        };
+        skyUniforms['topColor'].value.copy(hemiLight.color);
+        const skyGeo = new THREE.SphereGeometry(200, 32, 15);
+        const skyMat = new THREE.ShaderMaterial({
+            uniforms: skyUniforms,
+            vertexShader: skyVertShader,
+            fragmentShader: skyFragShader,
+            side: THREE.BackSide
+        });
+        const sky = new THREE.Mesh(skyGeo, skyMat);
+        this.group.add(sky);
+    }
+}
