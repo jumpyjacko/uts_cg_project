74ef907c91a601d17674c71060e018e53ee85d6d
Jackson Ly <bobjacklee27@gmail.com>
2026-05-10 13:00:32 +1000

    add: icons for house and lighthouse

--- src/ui/icons.js
+++ src/ui/icons.js
@@ -30,8 +30,15 @@ document.querySelectorAll('.three-dock-item').forEach((el) => {
     views.push(view);
 
     switch (action) {
-        case "tree":
+        case 'tree':
             loadModel("./models/tree1.glb", view);
+            break;
+        case 'house':
+            loadModel("./models/house4.glb", view);
+            break;
+        case 'lighthouse':
+            loadModel("./models/lighthouse.glb", view);
+            break;
         default:
     }
 });
