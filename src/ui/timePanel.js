a9af0d468623bef21de11d1713ec280dd6e7c580
Jackson Ly <bobjacklee27@gmail.com>
2026-05-29 13:57:35 +1000

    add: speedup button

--- src/ui/timePanel.js
+++ src/ui/timePanel.js
@@ -1,17 +1,42 @@
+const timescale = new Proxy({
+    timescale: 1,
+}, {
+    set(target, property, value) {
+        target[property] = value;
+
+        if (property === 'timescale') {
+            let timeElement = document.getElementById('current-timescale');
+            timeElement.textContent = value;
+        }
+
+        return true;
+    }
+})
+
 let paused = false;
 
 export function initTimePanel(world) {
     const pauseBtn = document.getElementById('pause-btn');
+    const speedupBtn = document.getElementById('speed-up-btn');
 
     if (pauseBtn) {
         pauseBtn.addEventListener('click', () => {
             paused = !paused;
 
             if (paused) {
-                world.timer.setTimescale(0);
+                timescale.timescale = 0;
             } else {
-                world.timer.setTimescale(1);
+                timescale.timescale = 1;
             }
+
+            world.timer.setTimescale(timescale.timescale); // NOTE: gross, im too lazy to change the name
+        })
+    }
+
+    if (speedupBtn) {
+        speedupBtn.addEventListener('click', () => {
+            timescale.timescale += 1;
+            world.timer.setTimescale(timescale.timescale);
         })
     }
 }
